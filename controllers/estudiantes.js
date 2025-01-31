e8de7e17fa3435837cae641f556fa30e2f25528e
Gerardo Aeschlimann <gaeschlimann700@gmail.com>
2025-01-30 18:15:11 -0600

    Router y controller agregado

---
+++ controllers/estudiantes.js
@@ -0,0 +1,9 @@
+const { request, response } = require("express");
+
+const getAllEstudiantes = async (request,response)=>{
+    response.status(200).json({
+        'status':200,
+        message:"Hello from server"
+    })
+}
+module.exports={getAllEstudiantes}
