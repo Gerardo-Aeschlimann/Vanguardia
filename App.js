4dccb96e340a4aa3421ee27b15ba155bd669af78
Gerardo Aeschlimann <gaeschlimann700@gmail.com>
2025-02-11 20:47:09 -0600

    Dotenv

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const app = express()
 const port =3000
+
+require('dotenv').config()
+console.log(process.env)
+
 app.get('/',(req,res)=>{
     res.send('Hello World')
 })
